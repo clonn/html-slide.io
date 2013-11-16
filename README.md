6c0bf058316e939dc32c35a042f1949ac95a89a8
clonn <clonncd@gmail.com>
2013-11-16 08:41:26 +0800

    update code

---
+++ README.md
@@ -0,0 +1,37 @@
+HTML5 Slide for Socket.IO
+=================================
+
+[HTML5 Slide](http://github.com/sioked/html5-slides) about [Socket.IO](http://github.com/LearnBoost/Socket.IO-node) for presentation in [FRENDS](http://frends.kr)
+
+## Requirements
+- node.js (tested in 0.6.18)
+- Socket.IO (tested in 0.9.6)
+
+## How to use
+run node server(you have to run server.js in socket.io directory)
+
+    node socketio-slide.js
+
+* connect at web browser - `http://localhost:3000/`
+* connect at client web browser - `http://localhost:3000/?id=user`
+* connect at admin web browser - `http://localhost:3000/?id=user&admin=true`
+
+### Explain
+
+This Slide is about `Socket.IO` using [HTML5 Slide](http://github.com/sioked/html5-slides).
+
+It contained some demo in slide.
+
+Actually two sockets is used in slide, one is for demo.
+another is for sync page of slide with others. page sync is operated only when id is matched. so you can connect to like `http://localhost:8124/?id=test` for sync page. then sync page with each other(eg. `?id=test`). if id isn't exist. slide doesn't sync.
+
+### Slide Scenario
+
+* connect to url at more than two browser(use `?id=sameid`)
+* explain slide (all slide synced page)
+* at `Communication with Server` page, demo for send(). your message will be received to your browser not other.
+* at `Broadcast via server` page, demo for broadcast(). your message will be broadcase to other browser(if those are connected), not your browser.
+
+### Demo
+
+* [Socket.IO : Real-Time Web for Everywhere](http://sideeffect.kr:8001/)
---
+++ app.js
@@ -0,0 +1,81 @@
+var http = require("http"),
+    util = require("util"),
+    url = require("url"),
+    fs = require("fs"),
+      path = require("path"),
+    io = require("socket.io"),
+    PORT = 3000,
+    HOST = null;
+
+server = http.createServer(function (req, res) {
+
+    var uri = url.parse(req.url).pathname;
+
+    switch (uri) {
+        case '/':
+        case '/main.html':
+            staticHandler("./main.html", req, res);
+            break;
+        case '/main.js':
+            staticHandler("./main.js", req, res);
+            break;
+        default:
+            var filename = path.join(process.cwd(), "./" + uri);
+            path.exists(filename, function(exists) {
+                if(!exists) {
+                    res.writeHead(404, {"Content-Type": "text/plain"});
+                    res.write("404 Not Found\n");
+                    res.end();
+                    return;
+                }
+
+                fs.readFile(filename, "binary", function(err, file) {
+                    if(err) {
+                        res.writeHead(500, {"Content-Type": "text/plain"});
+                        res.write(err + "\n");
+                        res.end();
+                        return;
+                    }
+
+                    res.writeHead(200);
+                    res.write(file, "binary");
+                    res.end();
+                });
+            });
+    }
+});
+server.listen(PORT, HOST);
+
+
+io = io.listen(server);
+
+io.sockets.on("connection", function(client) {
+    client.on("message", function(msg) {
+      if (msg.slide) {
+        client.broadcast.json.send(msg);
+      }
+      if (msg.type === "send") {
+        client.json.send( { "text": msg.text, "type":"send" } );
+      } else if (msg.type === "broad") {
+        client.broadcast.json.send( { "text": msg.text, "type":"broad" } );
+      }
+    });
+});
+
+var staticHandler = function(filename, req, res) {
+    var body;
+
+    fs.readFile(filename, function(err, data) {
+        if (err) {
+            util.debug('Error loading file ' + filename);
+        } else {
+            body = data;
+        }
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+            'Content-Length': body.length
+        });
+        res.write(body);
+        res.end();
+    });
+};
